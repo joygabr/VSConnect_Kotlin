8169de2bfb99e9f7b6ccf812b36c103649c3222e
Joy Gabriella <joysan05@outlook.com>
2024-01-18 21:53:21 -0300

    Atualizado

--- app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
@@ -2,8 +2,11 @@ package com.senai.vsconnect_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.senai.vsconnect_kotlin.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
